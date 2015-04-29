3a6aa9f0e43cf01bad33c141bab57981789e6848
Kevin Wilson <kevin@streamba.net>
2015-04-29 17:07:38 +0100

    Removed generated files.

--- FluxTests/app/eventTypes.js
+++
@@ -1,9 +0,0 @@
-var FluxTests;
-(function (FluxTests) {
-    (function (EventType) {
-        EventType[EventType["ItemAdded"] = 0] = "ItemAdded";
-        EventType[EventType["ItemSubmitted"] = 1] = "ItemSubmitted";
-    })(FluxTests.EventType || (FluxTests.EventType = {}));
-    var EventType = FluxTests.EventType;
-})(FluxTests || (FluxTests = {}));
-//# sourceMappingURL=eventTypes.js.map
